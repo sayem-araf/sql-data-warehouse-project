436f5eec15060189231d20f50100a811f712418c
Sayem Araf <139066946+sayem-araf@users.noreply.github.com>
2026-04-05 18:46:21 +0300

    Create 'Datawarehouse' database with schemas
    This script creates a new SQL Server database named 'Datawarehouse' with bronze, silver, and gold schemas, following the medallion architecture.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,27 @@
+/*-- Create Database 'Datawarehouse'
+==========================================
+A SQL Server-based data warehouse built on the medallion architecture (bronze, silver, gold schemas). 
+This project provides a structured approach to data ingestion, transformation, and analytics-ready data layers.
 
+WARNING:
+  Running this will drop the entire 'DataWarehouse' database if it exists.
+  All data in the database will be permanently deleted. Proceed with caution and ensure you have proper backups before running this script.
+*/
+
+USE master;
+GO
+
+CREATE DATABASE Datawarehouse;
+GO
+
+USE Datawarehouse;
+GO
+
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
