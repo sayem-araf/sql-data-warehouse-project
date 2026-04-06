8415847ec386c780ee0a637f9324d6497f23c6e3
Sayem Araf <139066946+sayem-araf@users.noreply.github.com>
2026-04-06 16:01:54 +0300

    Update database initialization script for new data sources

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -97,8 +97,55 @@ CREATE TABLE bronze.erp_px_cat_g1v2 (
 );
 
 -- Uploading Data
+
+TRUNCATE TABLE bronze.crm_cust_info;
 BULK INSERT bronze.crm_cust_info
-FROM 'Datasource/datapath/mountedformac'
+FROM 'datasets/source_crm/cust_info.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+
+TRUNCATE TABLE bronze.crm_prd_info;
+BULK INSERT bronze.crm_prd_info
+FROM 'datasets/source_crm/prd_info.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+
+TRUNCATE TABLE bronze.crm_sales_details;
+BULK INSERT bronze.crm_sales_details
+FROM 'datasets/source_crm/sales_details.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+
+TRUNCATE TABLE bronze.erp_cust_az12;
+BULK INSERT bronze.erp_cust_az12
+FROM 'datasets/source_erp/cust_az12.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+
+TRUNCATE TABLE bronze.erp_loc_a101;
+BULK INSERT bronze.erp_loc_a101
+FROM 'datasets/source_erp/loc_a101.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+
+TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+BULK INSERT bronze.erp_px_cat_g1v2
+FROM 'datasets/source_erp/px_cat_g1v2.csv'
 WITH (
     FIRSTROW = 2,
     FIELDTERMINATOR = ',',
