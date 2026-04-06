b2087d48ec3357fd232c4dbdfce27345e1fc65c9
Sayem Araf <139066946+sayem-araf@users.noreply.github.com>
2026-04-06 15:23:39 +0300

    Add BULK INSERT for customer data from CSV
    Added a BULK INSERT statement to load customer data from a CSV file into the crm_cust_info table.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -95,3 +95,13 @@ CREATE TABLE bronze.erp_px_cat_g1v2 (
     subcat NVARCHAR(50),
     maintenance NVARCHAR(50)
 );
+
+-- Uploading Data
+BULK INSERT bronze.crm_cust_info
+FROM '/Users/sayemaraf/Desktop/sql-data-warehouse-project/datasets/source_crm/cust_info.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+
